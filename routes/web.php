524df7638cac08e5e18030efb2662418f756e83f
vagrant <vagrant@homestead>
2020-03-24 08:58:50 +0000

    初始化样式

--- routes/web.php
+++ routes/web.php
@@ -28,3 +28,15 @@ Route::get('/about', function () {
     return view('about');
 });
  **/
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
